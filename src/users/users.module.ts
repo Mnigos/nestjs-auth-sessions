8a6674469cb360154f4ac69448b1ab9689de12e1
MoneyIgos <makowskiigor@gmail.com>
2021-08-18 16:59:56 +0200

    feat(users.module.ts): add MongooseModule configuration

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common'
+import { MongooseModule } from '@nestjs/mongoose'
 
+import { UserSchema } from './users.schema'
 import { UsersService } from './users.service'
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: 'Users', schema: UserSchema }])],
   providers: [UsersService],
+  exports: [UsersService],
 })
 export class UsersModule {}
