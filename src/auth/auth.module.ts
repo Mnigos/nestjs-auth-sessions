c3d7e62abb3894026d538b3f93463f0003792aa5
MoneyIgos <makowskiigor@gmail.com>
2021-08-18 18:46:16 +0200

    feat(auth): add PassportModule configuration

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common'
+import { PassportModule } from '@nestjs/passport'
+
+import { UsersModule } from '../../users/users.module'
 
 import { AuthService } from './auth.service'
 import { AuthController } from './auth.controller'
 
 @Module({
+  imports: [
+    UsersModule,
+    PassportModule.register({
+      sessions: true,
+    }),
+  ],
   providers: [AuthService],
   controllers: [AuthController],
 })
