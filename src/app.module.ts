1196dae05e7975d78b9b588821a5f70dd5330d85
MoneyIgos <makowskiigor@gmail.com>
2021-08-18 16:40:56 +0200

    feat(app.module): add ConfigModule and MongooseModule  configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { MongooseModule } from '@nestjs/mongoose'
 import { UsersModule } from './users/users.module'
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URI),
+    UsersModule,
+  ],
 })
 export class AppModule {}
