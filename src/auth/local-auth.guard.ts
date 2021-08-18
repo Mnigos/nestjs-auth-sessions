8860ccdd744cc08aaf2cef5d53eba70796fb7d02
MoneyIgos <makowskiigor@gmail.com>
2021-08-18 21:02:20 +0200

    feat(auth): create LocalAuthGuard

---
+++ src/auth/local-auth.guard.ts
@@ -0,0 +1,13 @@
+import { Injectable, ExecutionContext, CanActivate } from '@nestjs/common'
+import { AuthGuard } from '@nestjs/passport'
+
+@Injectable()
+export class LocalAuthGuard extends AuthGuard('local') implements CanActivate {
+  async canActivate(context: ExecutionContext) {
+    const result = (await super.canActivate(context)) as boolean
+    const request = context.switchToHttp().getRequest()
+
+    await super.logIn(request)
+    return result
+  }
+}
