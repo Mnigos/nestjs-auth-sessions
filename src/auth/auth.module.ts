1c01da54d5759af1a541ba3709448587fda6bf26
MoneyIgos <makowskiigor@gmail.com>
2021-08-18 20:35:29 +0200

    style(auth): change import order in AuthModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common'
 import { PassportModule } from '@nestjs/passport'
 
-import { UsersModule } from '../../users/users.module'
+import { UsersModule } from '../users/users.module'
 
 import { AuthService } from './auth.service'
 import { AuthController } from './auth.controller'
