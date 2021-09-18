e810ac408e7b9b57f65920b48d29a7239fab3f1e
Mnigos <makowskiigor@gmail.com>
2021-09-18 11:10:43 +0200

    refactor(AuthService): remove login method

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,19 +1,14 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common'
-import { JwtService } from '@nestjs/jwt'
 import { compare } from 'bcrypt'
 
 import { User } from '../users/interfaces/user.interface'
 import { UsersService } from '../users/users.service'
 
-import { Credentials } from './interfaces/credentials.interface'
 import { UserToReturn } from './interfaces/user-to-return.interface'
 
 @Injectable()
 export class AuthService {
-  constructor(
-    private readonly usersService: UsersService,
-    private readonly jwtService: JwtService
-  ) {}
+  constructor(private readonly usersService: UsersService) {}
 
   async validateUser(user: User): Promise<UserToReturn> {
     const { name, pass, ...rest } = user
@@ -25,24 +20,6 @@ export class AuthService {
     return {
       name,
       ...rest,
-    }
-  }
-
-  async login(credentials: Credentials): Promise<{ access_token: string }> {
-    const { nameOrEmail } = credentials
-
-    const emailRegex = /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/
-
-    const foundedUser = nameOrEmail.match(emailRegex)
-      ? await this.usersService.getOneByEmail(nameOrEmail)
-      : await this.usersService.getOneByName(nameOrEmail)
-
-    const { name, _id } = await this.usersService.getOneByName(foundedUser.name)
-
-    const payload = { name, sub: _id }
-
-    return {
-      access_token: this.jwtService.sign(payload),
     }
   }
 }
