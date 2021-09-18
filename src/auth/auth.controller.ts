0929c28a6ce93188fed3258d8578da4c68332a90
Mnigos <makowskiigor@gmail.com>
2021-09-18 11:13:30 +0200

    feat(AuthController): handle login and logout methods

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common'
+import { Controller, Get, Request, Post, UseGuards } from '@nestjs/common'
+
+import { LocalAuthGuard } from './local-auth.guard'
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  login(): { success: boolean } {
+    return { success: true }
+  }
+
+  @Get('logout')
+  logout(@Request() request): { success: boolean } {
+    request.logout()
+
+    return { success: true }
+  }
+}
