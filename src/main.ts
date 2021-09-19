05d3a5e695baf96ca00972310e5b14d4b9ae3767
Mnigos <makowskiigor@gmail.com>
2021-09-19 16:07:46 +0200

    chore(config): create config for sessions

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import * as dotenv from 'dotenv'
 dotenv.config()
 import { NestFactory } from '@nestjs/core'
 import { InternalServerErrorException } from '@nestjs/common'
+import * as passport from 'passport'
+import * as session from 'express-session'
 
 import { AppModule } from './app.module'
 
@@ -10,6 +12,23 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule, {
       cors: true,
     })
+
+    app.use(
+      session({
+        name: 'connect.sid',
+        secret: process.env.JWT_SECRET,
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+          secure: false,
+          httpOnly: false,
+          maxAge: 1000 * 60 * 60 * 30 * 24,
+        },
+      })
+    )
+    app.use(passport.initialize())
+    app.use(passport.session())
+
     await app.listen(process.env.PORT || 3000)
   } catch {
     throw new InternalServerErrorException()
